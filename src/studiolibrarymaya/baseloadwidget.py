51f688c21c4959768e277c01c8bfc62fb3cb64ae
Jerome Eippers <jeromee@nwavedigital.com>
2019-05-13 17:12:31 +0200

    Fix crash when closing selectionset

--- src/studiolibrarymaya/baseloadwidget.py
+++ src/studiolibrarymaya/baseloadwidget.py
@@ -254,7 +254,8 @@ class BaseLoadWidget(QtWidgets.QWidget):
         :rtype: None
         """
         self.setScriptJobEnabled(False)
-        self.item().saveOptions(**self._optionsWidget.optionsToDict())
+        if self._optionsWidget:
+            self.item().saveOptions(**self._optionsWidget.optionsToDict())
         QtWidgets.QWidget.close(self)
 
     def scriptJob(self):
